d19b8d4c47e03bae8fd4114b430792a886379129
Danté Kellman-Thompson <dante.kellman@icloud.com>
2025-06-03 17:59:22 +0100

    added to the code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,28 @@
 import React from "react";
 import "./App.css";
-import Banner from "../components/Banner/Banner";
+import Card from "../components/Card/Card";
+// import Banner from "../components/Banner/Banner";
 // import Badge from "../components/Badge/Badge";
 
 function App() {
   return (
     <main>
-      <Banner color="green">Congratulations!</Banner>
-      <Banner color="yellow">Attention</Banner>
-      <Banner color="red">There is not a problem with your application</Banner>
-      <Banner color="blue">Update available</Banner>
+      <Card></Card>
     </main>
   );
 }
+
+// THIS LINKS TO THE BANNER COMPONENT.
+// function App() {
+//   return (
+//     <main>
+//       <Banner color="green">Congratulations!</Banner>
+//       <Banner color="yellow">Attention</Banner>
+//       <Banner color="red">There is not a problem with your application</Banner>
+//       <Banner color="blue">Update available</Banner>
+//     </main>
+//   );
+// }
 
 // THIS LINKS TO THE BADGE COMPONENT.
 // function App() {
